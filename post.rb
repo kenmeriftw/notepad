7f6cad64ee19d7cd8e540c1749f015b2917b9e1a
knmrftw <2669997@gmail.com>
2021-02-26 23:41:40 +0300

    Notepad v.1, working

--- post.rb
+++ post.rb
@@ -1,25 +1,31 @@
 
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
   end
 
   def read_from_console
-
   end
 
   def to_strings
-
   end
 
   def save
-    file = File.new(file_path)
+    file = File.new(file_path, 'w:UTF-8')
 
-    for item in to_strings do
+    to_strings.each { |item|
       file.puts(item)
-    end
+    }
 
     file.close
   end
